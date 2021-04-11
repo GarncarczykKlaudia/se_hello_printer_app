82b3ff95e35aa093bec2daea53b2d8ccc8869830
GarncarczykKlaudia <GarncarczykKlaudia@users.noreply.github.com>
2021-04-11 10:54:21 +0000

    dodatnie testu json

--- test/test_views.py
+++ test/test_views.py
@@ -1,6 +1,8 @@
 import unittest
+import json
 from hello_world import app
 from hello_world.formater import SUPPORTED
+
 
 
 class FlaskrTestCase(unittest.TestCase):
@@ -14,8 +16,18 @@ class FlaskrTestCase(unittest.TestCase):
         ','.join(SUPPORTED) in s
 
     def test_msg_with_output(self):
+        test_data={"imie":"Klaudia", "msg":"Hello World!"}
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Klaudia", "mgs":"Hello World!"}', rv.data)
+        js=json.loads(rv.data)
+        self.assertEqual(test_data['msg'],js['msg'])
+        self.assertEqual(test_data['imie'],js['imie'])
+        self.assertEqual(len(test_data), len(js))
+
+
+
+        #rv.data.imie = 'Klaudia'
+        #rv.data.msg = 'Hello World!'
+        #self.assertEqual(b'{ "imie":"Klaudia", "mgs":"Hello World!"}', rv.data)
 
     def test_msg_with_output2(self):
         rv = self.app.get('/?output=xml')
